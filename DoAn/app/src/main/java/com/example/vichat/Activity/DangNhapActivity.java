b5f18d7eda854c052688e2fa053428d63380ce08
vanthanh0309 <45441620+vanthanh0309@users.noreply.github.com>
2019-11-28 15:16:58 +0700

    dangnhapfb

--- DoAn/app/src/main/java/com/example/vichat/Activity/DangNhapActivity.java
+++ DoAn/app/src/main/java/com/example/vichat/Activity/DangNhapActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +27,9 @@ import com.example.vichat.menuActivity;
 
 import org.json.JSONObject;
 
+import java.security.MessageDigest;
+import java.security.MessageDigestSpi;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.Delayed;
 
 import javax.xml.transform.Result;
@@ -67,12 +74,29 @@ public class DangNhapActivity extends AppCompatActivity implements View.OnClickL
         });
         btnDangNhap = findViewById(R.id.btnDangNhap);
         btnDangNhap.setOnClickListener(this);
+        printKeyHash();
     }
+    private void printKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.example.vichat", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
 
-    @Override
-    public void onClick(View v) {
-        signIn();
-    }
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }    }
+
+        @Override
+        public void onClick (View v){
+            signIn();
+        }
 
     private void signIn() {
         //btnDangNhap.setVisibility(View.GONE);
